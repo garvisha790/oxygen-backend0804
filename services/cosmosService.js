6b7a11691b6617e2789b9ac425ef278609058e5f
Garvisha Bansal <garvisha.bansal@apphelix.ai>
2025-04-10 12:52:45 +0530

    Commit Commit

--- services/cosmosService.js
+++ services/cosmosService.js
@@ -10,12 +10,12 @@ const getTelemetryDataByDeviceName = async (deviceName , container) => {
       // Fetch telemetry data for the given device name
       console.log(`📡 Fetching telemetry data for device: ${deviceName}`);
       const querySpec = {
-          query: "SELECT * FROM c WHERE c.device = @deviceName ORDER BY c.timestamp DESC",
+          query: "SELECT * FROM c WHERE c.device = @deviceName AND c.temperature>=0 OR c.temperature<0 AND c.humidity>=0 AND c.oilLevel>=0 ORDER BY c.timestamp DESC",
           parameters: [{ name: "@deviceName", value: deviceName }]
       };
       const { resources } = await container.items.query(querySpec).fetchAll();
-        console.log("Telemetry Data:", resources[1]);
-      return resources.length > 0 ? resources[1] : null;
+        console.log("Telemetry Data:", resources[0]);
+      return resources.length > 0 ? resources[0] : null;
   } catch (error) {
       console.error("❌ Error Fetching Telemetry Data:", error.message);
       return null;
